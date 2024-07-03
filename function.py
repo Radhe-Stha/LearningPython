511a478949f901418aa25f8f5dd505d9578c3652
Radhe <aasmin0070608@gmail.com>
2024-07-03 19:04:39 +0545

    function

---
+++ function.py
@@ -0,0 +1,25 @@
+def display_address():
+    print(f"My address is Bhaktapur")
+
+
+display_address()
+
+def display_even(first, last):
+    for i in range(first, last + 1):
+        if i % 2 == 0:
+            print(i)
+
+
+start =  100
+end = 150
+display_even(start, end)
+
+#types of function
+# 1. system defined
+# 2. user defined
+
+# also 4 types
+# 1. No parameter and no return
+# 2. parameter and no return
+# 3.  parameter and  return
+# 4.  parameter and no return
